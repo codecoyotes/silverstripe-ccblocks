3b8e618ccfb5d2a28a4911b10d0c45ca9005cdfe
Michel van der Steege <Michel@michel-pc.(none)>
2017-02-05 14:47:14 +0100

    Block size

--- code/extensions/BlockExtension.php
+++ code/extensions/BlockExtension.php
@@ -1,36 +1,43 @@
 <?php
 
-class CCBlockExtension extends DataExtension
-{
+class CCBlockExtension extends DataExtension {
 
-	private static $db = array(
-		'ShowTitle' => 'Boolean',
-		'BackgroundColorClass' => 'Varchar',
-		'BackgroundOpacityClass' => 'Varchar',
-		'BackgroundAltClass' => 'Boolean',
-		'FullWidth' => 'Boolean'
-	);
+		private static $db = array(
+				'ShowTitle' => 'Boolean',
+				'BackgroundColorClass' => 'Varchar',
+				'BackgroundOpacityClass' => 'Varchar',
+				'BackgroundAltClass' => 'Boolean',
+				'FullWidth' => 'Boolean',
+				'BlockSize' => 'Varchar'
+		);
+		private static $has_one = array(
+				'BackgroundImage' => 'Image'
+		);
+		private static $block_sizes = array(
+				'' => 'default',
+				'block-xl' => 'Block XL',
+				'block-lg' => 'Block LG',
+				'block-md' => 'Block MD',
+				'block-sm' => 'Block SM',
+				'block-xs' => 'Block XS'
+		);
 
-	private static $has_one = array(
-		'BackgroundImage' => 'Image'
-	);
-
-	public function updateCMSFields(FieldList $fields)
-	{
-		$fields->addFieldToTab('Root.Background', $backgroundImageField = UploadField::create('BackgroundImage'));
-		$backgroundColorClasses = Config::inst()->get('CCBlocks', 'backgroundcolorclasses');
-		$backgroundOpacityClasses = Config::inst()->get('CCBlocks', 'backgroundopacityclasses');
-		if($backgroundColorClasses){
-			$fields->addFieldToTab('Root.Background', DropdownField::create('BackgroundColorClass', 'Background color', $backgroundColorClasses));
-		}else{
-			$fields->addFieldToTab('Root.Background', TextField::create('BackgroundColorClass', 'Background color'));
+		public function updateCMSFields(FieldList $fields) {
+				$fields->addFieldToTab('Root.Main', new DropdownField('BlockSize', 'Block size', self::$block_sizes));
+				$fields->addFieldToTab('Root.Background', $backgroundImageField = UploadField::create('BackgroundImage'));
+				$backgroundColorClasses = Config::inst()->get('CCBlocks', 'backgroundcolorclasses');
+				$backgroundOpacityClasses = Config::inst()->get('CCBlocks', 'backgroundopacityclasses');
+				if ($backgroundColorClasses) {
+						$fields->addFieldToTab('Root.Background', DropdownField::create('BackgroundColorClass', 'Background color', $backgroundColorClasses));
+				} else {
+						$fields->addFieldToTab('Root.Background', TextField::create('BackgroundColorClass', 'Background color'));
+				}
+				if ($backgroundOpacityClasses) {
+						$fields->addFieldToTab('Root.Background', DropdownField::create('BackgroundOpacityClass', 'Background opacity', $backgroundOpacityClasses));
+				} else {
+						$fields->addFieldToTab('Root.Background', TextField::create('BackgroundOpacityClass', 'Background opacity'));
+				}
+				$fields->addFieldToTab('Root.Background', CheckboxField::create('BackgroundAltClass', 'Add background alt class'));
 		}
-		if($backgroundOpacityClasses){
-			$fields->addFieldToTab('Root.Background', DropdownField::create('BackgroundOpacityClass', 'Background opacity', $backgroundOpacityClasses));
-		}else{
-			$fields->addFieldToTab('Root.Background', TextField::create('BackgroundOpacityClass', 'Background opacity'));
-		}
-		$fields->addFieldToTab('Root.Background', CheckboxField::create('BackgroundAltClass', 'Add background alt class'));
-	}
 
 }
--- templates/Includes/BlockArea.ss
+++ templates/Includes/BlockArea.ss
@@ -1,6 +1,6 @@
 <div class="block_area block_area_$AreaID.LowerCase" data-areaid="$AreaID">
     <% loop BlockArea %>
-		<div class="block $BackgroundColorClass $BackgroundOpacityClass<% if $BackgroundAltClass %> bg-alt<% end_if %><% if $BackgroundImage %> visual visual-lg visual-overlay parallax<% end_if %>"
+		<div class="block $BlockSize $BackgroundColorClass $BackgroundOpacityClass<% if $BackgroundAltClass %> bg-alt<% end_if %><% if $BackgroundImage %> visual visual-lg visual-overlay parallax<% end_if %>"
              <% if $BackgroundImage %>style="background-image: url('$BackgroundImage.URL')"<% end_if %>>
             <% if not $FullWidth %>
 			<div class="container">
